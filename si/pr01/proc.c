20eb4f41f47c7c56a8fa91492b5c016ba3d403ad
jdell <joemismito@gmail.com>
2014-02-13 18:24:38 +0000

    Some refactoring

--- si/pr01/proc.c
+++ si/pr01/proc.c
@@ -6,7 +6,7 @@
 *Email: wilton.castro@udc.es
 *
 */
-
+ 
 #include <stdio.h>
 
 #define MAX_LETTERS 30
@@ -21,9 +21,22 @@ char *current_statement, *next_statement;
 char *current_letter;
 
 void main()
-{  
-   int readbytes = read(0, file, MAX_FILE_LENGTH)>0;
-   printf("");
-  
+{
+	int readbytes = read(0, file, MAX_FILE_LENGTH)>0;
+	if (readbytes)
+	{
+		printf("%s\n", file);
+		current_statement = strtok(file, ".\n");
+		int i =  0;
+		while(current_statement!=NULL)
+		{
+			//next_statement = strtok(NULL, ".");
+			i++;
+			printf("%d - %s\n", i, current_statement);
+
+			current_statement = strtok(NULL, ".\n");
+		}
+	}
+	printf("\n");
 }
 
